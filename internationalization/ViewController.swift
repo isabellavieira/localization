d0ede22f589a4896a00f84e1ee524ee7050ce37e
isabellavieira57 <isabellavieira57@gmail.com>
2016-08-25 09:50:46 -0300

    Initial Commit

---
+++ internationalization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  internationalization
+//
+//  Created by Isabella Vieira on 8/25/16.
+//  Copyright © 2016 Isabella Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
